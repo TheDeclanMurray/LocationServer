75b4e2272cf77bba57244c8d4745d9b9a06c7176
Loaner <loaner@clients.finalsitecdn.com>
2022-04-27 14:38:31 -0700

    pushing from MAC

--- main.py
+++ main.py
@@ -9,19 +9,43 @@ import json
 
 from ledger import Ledger
     
-def Listener(threadName):
-    global ISRUNNING
-    print("Listerner() running")
 
-    DiscoverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    DiscoverSocket.bind(("127.0.0.1",Port))
-    activeSockets.append(DiscoverSocket)
+Host = "127.0.0.1"
+PORT = 9090
+MULTI_CAST = ("224.0.0.1",PORT)
+ISRUNNING = True
+LEDGER = Ledger()
+ACTIVESOCKETS = []
+TIMEOUT = 0.02
+
+def ReadDiscover(threadName):
+    global ISRUNNING
+    print("ReadDiscover() running")
+
+    DiscoverListener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    DiscoverListener.bind(('',PORT))
+    ACTIVESOCKETS.append(DiscoverListener)
 
     while ISRUNNING:
         
-        packet, (b, a) = DiscoverSocket.recvfrom(4096)
-        packet = json.loads(packet.decode)
-        print("Discover:", b)
+        try:
+            data, (ip, port) = DiscoverListener.recvfrom(4096)
+        except socket.timeout as e:
+            continue 
+        print("Receaved Discover")
+        try:
+            data = json.loads(data.decode())
+            print("M:", data, "from", ip)
+        except Exception as e:
+            print("Error:", e)
+
+        LEDGER.add(ip,ip)
+
+
+        # HANDLE the HELLOW responce 
+
+
+        
 
         # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         #     s.bind((Host,Port))
@@ -40,7 +64,7 @@ def Listener(threadName):
         # s.close()
 
 
-def Discover(threadName):
+def SendDiscover(threadName):
     print("Discover() running")
 
     DiscoverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -49,7 +73,7 @@ def Discover(threadName):
     ttl = struct.pack('b',1)
     DiscoverSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-    activeSockets.append(DiscoverSock)
+    ACTIVESOCKETS.append(DiscoverSock)
 
     def repeat():
         sendMulticast(DiscoverSock)
@@ -60,49 +84,57 @@ def Discover(threadName):
     t.start()
 
 def sendMulticast(sock):
-    Jobj = json.dumps({"type":"DISCOVER","time":int(time.time()),"port":Port})
-    Jobj = bytes(Jobj, "utf-8")
-    sent = sock.sendto(Jobj,multiCast)
+    Jobj = json.dumps({"type":"DISCOVER","time":int(time.time()),"port":PORT})
+    # Jobj = bytes(Jobj, "utf-8")
+    
+    sent = sock.sendto(Jobj.encode(),MULTI_CAST)
     print("Sending DISCOVER",sent)
+
+def SendHello():
+
+    sendIP = host
+    msg = {"type":"HELLO","port":PORT,"clients":getClients()}
+    msg = json.dumps(msg)
+    helloSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    helloSocket.connect((sendIP, PORT))
+    helloSocket.send(msg.encode())
+
+def getClients():
+    keys = LEDGER.keys()
+    rtn = []
+    for key in keys:
+        rtn.append(key)
+    return rtn
+
+def ReadHello():
+    pass
+
+
 
 def exit_handler():
     global ISRUNNING 
-    ISRUNNING= False
+    ISRUNNING = False
 
-    for sock in activeSockets:
+    for sock in ACTIVESOCKETS:
         sock.close()
 
     for a in threading.enumerate():
         if a != threading.currentThread():
             a.join()
 
-
-def on_press(key):
-    try:
-        k = key.char
-    except:
-        k = key.name
-    if k == "C":
-        return False
         
 if __name__ == "__main__":
 
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
-    Host = "127.0.0.1"
-    Port = 9090
-    multiCast = ("224.0.0.1",Port)
-    ISRUNNING = True
-    ledger = Ledger()
-    ledger.add("killer","temp")
-    activeSockets = []
+    
 
     atexit.register(exit_handler)
 
 
-    DiscoverThread = threading.Thread(target=Discover,args=("1"))
+    DiscoverThread = threading.Thread(target=SendDiscover,args=("1"))
     # ListenerThread = threading.Thread(target=Listener,args=("2")) #the main thread
 
     DiscoverThread.start()
-    Listener("2")
+    ReadDiscover("2")
