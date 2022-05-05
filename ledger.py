46c6832c9a489d889e5bd00fa186cc92e78fcbc6
Declan <declanm2022>
2022-05-05 14:47:30 -0700

    Finished lab

--- ledger.py
+++ ledger.py
@@ -4,42 +4,52 @@ READERS = 8
 Sem = Semaphore(READERS)
 Mtx = Lock()
 
-
 class Ledger:
 
     """
-            0              1                 2
+         Key              0                 1
     [ [IP Address] [IP Connection] [Min to Experation] ]
     [ [          ] [             ] [                 ] ]
     ...
     """
 
-    def add(self, addy, con):
+    def add(self, addy, con, num=0):
         global READERS, Sem, Mtx
+        # grab all the semifors before editing
         Mtx.acquire()
         for i in range(READERS):
             Sem.acquire()
-        self.ledger[addy] = [con,5]
+
+        # handle different condiitons to adjust time of death
+        if self.ledger.__contains__(addy) and num==0:
+            num = self.ledger[addy][1]
+        else:
+            num = 5
+
+        # add or update the addy
+        self.ledger[addy] = [con,num]
+
+
+        # release the semifors and mutex
         for i in range(READERS):
             Sem.release()
         Mtx.release()
 
     def onMin(self):
         global READERS, Sem, Mtx
+        # grab the sems to edit
         Mtx.acquire()
         for i in range(READERS):
             Sem.acquire()
 
-
         delKeys = []
-
         # for every key decrement the time and delete if it runs out
         keys = self.ledger.keys()
         for key in keys:
             prev = self.ledger[key]
             prev[1] = prev[1] -1
 
-            if prev[1 <= 0]:
+            if prev[1] <= 0:
                 delKeys.append(key)
 
             self.ledger[key] = prev
@@ -47,16 +57,42 @@ class Ledger:
         for key in delKeys:
             self.ledger.pop(key)
 
+        # release the sems
         for i in range(READERS):
             Sem.release()
         Mtx.release()
 
     def readKeys(self):
-
+        # read the keys
         Mtx.acquire()
         Sem.acquire()
         Mtx.release()
         rtn = self.ledger.keys()
+        Sem.release()
+        
+        return rtn
+
+    def print(self):
+        rtn = "=============LEDGER================"
+
+        # get a semifore because I'm reading
+        Mtx.acquire()
+        Sem.acquire()
+        Mtx.release()
+
+        keys = self.ledger.keys()
+
+        # format ledger to return string
+        for key in keys:
+            line = "\n  "
+            line += key + (15-len(key))*" " + "      "
+            Fip = self.ledger.get(key)[0]
+            tm = self.ledger.get(key)[1]
+            line += (15-len(Fip))*" " + Fip + "   "
+            line += str(tm)
+            rtn += line
+
+        # release sem
         Sem.release()
         
         return rtn
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import atexit
 import logging
-from multiprocessing import Lock 
 import threading
 import time
 import socket
 import struct
 import json
+import signal
 
 from ledger import Ledger
     
 
-Host = "127.0.0.1"
+HOST = "127.0.0.1"
 PORT = 9090
 MULTI_CAST = ("224.0.0.1",PORT)
 ISRUNNING = True
@@ -19,122 +18,200 @@ ACTIVESOCKETS = []
 TIMEOUT = 0.02
 
 def ReadDiscover(threadName):
-    global ISRUNNING
-    print("ReadDiscover() running")
+    global ISRUNNING, LEDGER, ACTIVESOCKETS
 
+    # Set up UDP Discover Socket for Reading Discovers
     DiscoverListener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     DiscoverListener.bind(('',PORT))
     ACTIVESOCKETS.append(DiscoverListener)
 
+    # run in a while loop so it can happen again
     while ISRUNNING:
         
         try:
+            # wait for someone to send a discover
             data, (ip, port) = DiscoverListener.recvfrom(4096)
+            data = json.loads(data.decode())
+            # print("M(D):", data, "from", ip)
+            LEDGER.add(ip,ip,5)
+
+            # send a hello message back to discover sender in a new thread
+            HelloThread = threading.Thread(target=SendHello,args=[ip])
+            HelloThread.start()
         except socket.timeout as e:
             continue 
-        print("Receaved Discover")
-        try:
-            data = json.loads(data.decode())
-            print("M:", data, "from", ip)
         except Exception as e:
-            print("Error:", e)
+            pass
 
-        LEDGER.add(ip,ip)
-
-
-        # HANDLE the HELLOW responce 
-
-
-        
-
-        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #     s.bind((Host,Port))
-        #     s.listen()
-        #     activeSockets.append(s)
-        #     conn, addr = s.accept()
-        #     with conn:
-        #         print(f"Connected by {addr}")
-        #         while True:
-        #             data = conn.recv(1024)
-        #             if not data:
-        #                 break
-        #             print("Receaving data:", data)
-                    
-        #             # conn.sendall(data) # why??
-        # s.close()
 
 
 def SendDiscover(threadName):
-    print("Discover() running")
+    global ACTIVESOCKETS
 
+    # Set up a socket to send discover messages
     DiscoverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     DiscoverSock.settimeout(0.2)
 
+    # set up destination to be a multicast
     ttl = struct.pack('b',1)
     DiscoverSock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
     ACTIVESOCKETS.append(DiscoverSock)
 
+    # repeat ever minute
     def repeat():
-        sendMulticast(DiscoverSock)
-        t = threading.Timer(10.0,repeat)
+        # send multicast
+        sendMulticast(DiscoverSock) 
+        # update the ledger times
+        LEDGER.onMin()
+
+        # repeat in a minute
+        t = threading.Timer(60.0,repeat)
         t.start()
     
-    t = threading.Timer(10.0,repeat)
-    t.start()
+    repeat()
+
 
 def sendMulticast(sock):
     Jobj = json.dumps({"type":"DISCOVER","time":int(time.time()),"port":PORT})
-    # Jobj = bytes(Jobj, "utf-8")
+    try:
+        sock.sendto(Jobj.encode(),MULTI_CAST)
+    except socket.error as e:
+        pass
+    except Exception as e:
+        pass
+
+
+def SendHello(targetIP):
+    global ACTIVESOCKETS
+    sendIP = targetIP
     
-    sent = sock.sendto(Jobj.encode(),MULTI_CAST)
-    print("Sending DISCOVER",sent)
-
-def SendHello():
-
-    sendIP = host
+    # create the message
     msg = {"type":"HELLO","port":PORT,"clients":getClients()}
     msg = json.dumps(msg)
+
+    # create the TCP socket
     helloSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    helloSocket.connect((sendIP, PORT))
-    helloSocket.send(msg.encode())
+
+    try:
+        # connect socket and send message
+        helloSocket.connect((sendIP, PORT))
+        helloSocket.send(msg.encode())
+    except socket.error as e:
+        pass
+    except Exception as e:
+        pass
+    helloSocket.close()
+
 
 def getClients():
-    keys = LEDGER.keys()
+    global LEDGER
+    # get the clients in Ledger
+    keys = LEDGER.readKeys()
     rtn = []
     for key in keys:
         rtn.append(key)
     return rtn
 
+
 def ReadHello():
-    pass
+    global ISRUNNING, ACTIVESOCKETS
+
+    # Set up Hello listener socket
+    HelloListener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    HelloListener.bind(('',PORT))
+    HelloListener.listen(10)
+    ACTIVESOCKETS.append(HelloListener)
+
+    while ISRUNNING:
+        
+        try:         
+            # accept a conection and handle the connected socket to a new thread
+            sock, (ip, port) = HelloListener.accept()
+            addClientThread = threading.Thread(target=addClients,args=(sock,ip))
+            addClientThread.start()
+        except socket.timeout as e:
+            continue 
+        except Exception as e:
+            pass
+
+    HelloListener.close()
+
+
+def addClients(sock,ip):
+    global LEDGER
+
+    # handle hello socket connection
+
+    try:
+        data = sock.recv(4096)
+        sock.close()
+        data = json.loads(data.decode())
+        # add clients to ledger
+        cls = data['clients']
+        for cl in cls:
+            LEDGER.add(cl,ip)
+
+        # print("M(H):", data, "from", ip)
+    except Exception as e:
+        pass
 
 
 
-def exit_handler():
-    global ISRUNNING 
+def exit_handler(num=0,num2=0):
+    print(" Exit_Handler() Running Give it a Sec")
+    global ISRUNNING, ACTIVESOCKETS
     ISRUNNING = False
 
+
+    # Send an empty UDP packet to myself to close the UDP socket
+    msg = ""
+    blankDiscover = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    blankDiscover.connect((HOST, PORT))
+    blankDiscover.send(msg.encode())
+    blankDiscover.close()
+
     for sock in ACTIVESOCKETS:
+        try:
+            # handle windows/LINUX
+            sock.shutdown(1)
+        except socket.error as e:
+            pass
+        # close all sockets
         sock.close()
 
     for a in threading.enumerate():
+        # merge all threads into current
         if a != threading.currentThread():
+            if isinstance(a,threading.Timer):
+                a.cancel()
             a.join()
+
+def handleInput():
+    global ISRUNNING
+
+    while ISRUNNING:
+        inp = input()
+
+        # take an input display ledger if "ledger"
+        # quit program if "quit"
+        if inp == "ledger":
+            print(LEDGER.print())
+        if str.lower(inp) == "quit":
+            exit_handler()
+            quit()
 
         
 if __name__ == "__main__":
 
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    signal.signal(signal.SIGINT,exit_handler)
 
-    
-
-    atexit.register(exit_handler)
-
-
+    # set up main threads
     DiscoverThread = threading.Thread(target=SendDiscover,args=("1"))
-    # ListenerThread = threading.Thread(target=Listener,args=("2")) #the main thread
+    HelloThread = threading.Thread(target=ReadHello)
+    InputThread = threading.Thread(target=handleInput)
 
+    # start main threads
     DiscoverThread.start()
+    HelloThread.start()
+    InputThread.start()
     ReadDiscover("2")
